4bf3b35d4ddf9d832144174943707c93e8025244
453759 <127659341+453759@users.noreply.github.com>
2025-02-06 19:45:29 +0800

    Update translation_config.py

--- project_config/translation_config.py
+++ project_config/translation_config.py
@@ -3,13 +3,13 @@ import configargparse
 def get_args():
     parser = configargparse.ArgParser(config_file_parser_class=configargparse.YAMLConfigFileParser)
     parser.add_argument('--txt_path', type=str,
-                        default="/home/star/Projects/g4/cy/AutomaticTranslateCorrection/data/diastolic_frames_R.txt",
+                        default="",
                         help="Path to the input text file.")
     parser.add_argument('--img_root', type=str,
-                        default='/home/star/Data/g4/two_position/',
+                        default='',
                         help="Root directory of images.")
     parser.add_argument('--data_csv', type=str,
-                        default="/home/star/Projects/g4/cy/AutomaticTranslateCorrection/data/data_attribute.csv",
+                        default="",
                         help="Path to the output CSV file.")
     parser.add_argument('--num_generations', type=int, default=50,
                         help="Number of generations for processing.")
@@ -24,7 +24,7 @@ def get_args():
     parser.add_argument('--output_img_path', type=str,
                         default="/home/star/Data/g4/six_position_move/")
     parser.add_argument('--output_txt_path', type=str,
-                        default='/home/star/Projects/g4/cy/AutomaticTranslateCorrection/data_R/ablation_experiment/different_frames/translation_12_frames.txt')
+                        default='')
     args = parser.parse_known_args()[0]
 
     return args
